ac01e09666ef2b3d1296aad801865df9e314f8a3
Jed T <doom.slaver2@gmail.com>
2024-08-25 13:01:47 +0800

    home page visuals

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('shop/', views.shop, name='shop'),
+    path('item/', views.item, name='item'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('order/', views.order, name='order'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+
+def shop(request):
+    return render(request, "shop.html")
+
+
+def item(request):
+    return render(request, "item.html")
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+def order(request):
+    return render(request, "order.html")
+
