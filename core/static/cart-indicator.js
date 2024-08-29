61041550d57d219a8ee3a89c868be42a44c8c95d
Jed T <doom.slaver2@gmail.com>
2024-08-29 08:49:04 +0800

    saving shoe pk to local memory

--- core/static/cart-indicator.js
+++ core/static/cart-indicator.js
@@ -5,14 +5,14 @@ function resetCart() {
 
     const cartIndicator = document.getElementById('cart-indicator');
     cartIndicator.classList.add('hidden');
+
+    cartItems = [];
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
 }
 
 window.onload = function() {
     const savedCounter = localStorage.getItem('cartCounter');
-    if (savedCounter) {
-        counter = parseInt(savedCounter, 10);
-        if (counter > 0) {
-            document.getElementById('cart-indicator').classList.remove('hidden');
-        }
+    if (savedCounter && parseInt(savedCounter, 10) > 0) {
+        document.getElementById('cart-indicator').classList.remove('hidden');
     }
 };
--- core/static/cart.js
+++ core/static/cart.js
@@ -1,12 +1,19 @@
 let counter = 0;
+let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 
 function handleButtonClick() {
+    const shoePk = document.getElementById('addToCartButton').getAttribute('data-shoe-pk');
+    cartItems.push(shoePk);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    
     counter += 1;
     localStorage.setItem('cartCounter', counter);
-    console.log(counter);
     document.getElementById('cart-indicator').classList.remove('hidden');
-
+    
     showNotification();
+
+    console.log(counter);
+    console.log(cartItems);
 }
 
 function showNotification() {
@@ -22,7 +29,6 @@ function closeNotification() {
     const notification = document.getElementById('notification');
     notification.classList.add('hidden');
 }
-
 
 document.getElementById('addToCartButton').addEventListener('click', handleButtonClick);
 document.getElementById('closeNotifButton').addEventListener('click', closeNotification);
